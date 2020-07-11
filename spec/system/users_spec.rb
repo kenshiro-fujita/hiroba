a5e1d5dafd149a8ee6ae95ff53086d573645af12
kenshiro-fujita <kogyanta.hazimeti@gmail.com>
2020-07-11 04:50:42 +0000

    homebrewインストール

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Users', type: :system do
+  before do
+    @user = FactoryBot.create(:himura)
+  end
+  it 'ルートからマイページに移動' do
+    visit '/'
+    click_link 'マイページ'
+  end
+end
