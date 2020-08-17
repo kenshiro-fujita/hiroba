52d0aae4930d9c2f0aa00e9c6e3420c2d8a4c34e
awsfujita <kogyanta.hazimeti@gmail.com>
2020-08-17 08:21:33 +0000

    puma設定

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,8 @@ bind "unix:///var/www/rails/hiroba/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV') { 'development' }
+environment ENV.fetch('RAILS_ENV') { 'production' }
+daemonize true
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
